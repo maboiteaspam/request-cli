f5aedc8a451d74237b6f5a019cc01633998075ba
Clement Auger <clement.ager@d1m.cn>
2015-04-29 10:16:03 +0800

    Implement more options, make it more close to curl

--- README.md
+++ README.md
@@ -2,43 +2,63 @@
 
 A cli program to execute http request with request/request node js module.
 
+In the fashion of the famous curl, with ease of NodeJS.
+
+
 # Install
 
 ```npm i request-cli -g```
 
+
+If i have not yet published this module to NPM, please use this syntax meanwhile.
+
+```npm i maboiteaspam/request-cli -g```
+
+
 # Options
 
 ```sh
-  Usage: req <url> [options]
-
-
-  Commands:
-
-    *   help
+  Usage: undefined <url> [options]
 
   req url
 
   Options:
 
-    -h, --help                   output usage information
-    -V, --version                output the version number
-    --raw                        Raw display, no pretty print
-    --body                       Display Body response
-    --reqheaders                 Display Request headers
-    -m --method <HTTPMethod>     Request HTTP method
-    -s --strictSSL <strict>      Require strict SSL
-    -c, --cookies <cookies>      Set cookie string, such "a=v1&b=v2", don t forget quotes on unix because of the &
-    -u, --useragent <UserAgent>  User agent to inject to your query
+    -h, --help                    output usage information
+    -V, --version                 output the version number
+    --raw                         Raw display, no pretty print
+    --body                        Display Body response
+    -i, --reqheaders              Display Request headers
+    -I, --resheaders              Display Response headers
+    -m, --method <HTTPMethod>     Request HTTP method
+    -X, --request <HTTPMethod>    Request HTTP method. (Curl Style)
+    -s, --strictSSL <strict>      Require strict SSL
+    -c, --cookies <cookies>       Set cookie string, such "a=v1&b=v2", don t forget quotes on unix because of the &
+    -u, --useragent <UserAgent>   User agent to inject to your query
+    -A, --user-agent <UserAgent>  User agent to inject to your query (Curl Style)
+    -b, --cookie <name=data>      Pass the data to the HTTP server as a cookie. (Curl Style)
+    -d, --data <data>             Sends the specified data in a POST request to the HTTP server, in the same way that a browser does when a user has filled in an HTML form and presses the submit button. (Curl Style)
+    -e, --referer <url>           Sends the "Referrer Page" information to the HTTP server. (Curl Style)
+    -H, --header <header>         Sends the "Referrer Page" information to the HTTP server. (Curl Style)
+    --data-raw <data>             This posts data similarly to --data but without the special interpretation of the @ character. (Curl Style)
+    --data-urlencode  <data>      This posts data, similar to the other --data options with the exception that this performs URL-encoding. (Curl Style)
+    -k, --insecure                Allow insecure ssl certificates. (Curl Style)
+    -L, --location                Follow redirects. (Curl Style)
+    -o, --output <file>           Output to specified file. (Curl Style)
+    -u, --user <user:password>    Output to specified file. (Curl Style)
+
 
 ```
+
 
 # Usage
 
 ```sh
-request http://some.com/url
-request http://some.com/url -u 'sme user agent'
+request http://some.com/curl
+req http://some.com/curl -u 'sme user agent'
 ```
+
 
 # Status
 
-In development. It needs some tests.
+In development. It needs to implement all listed options above.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var pkg = require('./package.json');
 
+var urlUtil = require('url');
 var program = require('commander');
 var log = require('npmlog');
 var request = require('request');
@@ -11,13 +12,27 @@ program.version(pkg.version)
   .description('req url')
   .option('--raw', 'Raw display, no pretty print')
   .option('--body', 'Display Body response')
-  .option('--reqheaders', 'Display Request headers')
-  .option('-m --method <HTTPMethod>', 'Request HTTP method')
-  .option('-s --strictSSL <strict>', 'Require strict SSL')
-  .option('-c, --cookies <cookies>', 'Set cookie string, such "a=v1&b=v2", don t forget quotes on unix because of the &')
+  .option('-i, --reqheaders', 'Display Request headers')
+  .option('-I, --resheaders', 'Display Response headers')
+  .option('-m, --method <HTTPMethod>', 'Request HTTP method')
+  .option('-X, --request <HTTPMethod>', 'Request HTTP method. (Curl Style)')
+  .option('-k, --insecure', 'Allow insecure ssl certificates. (Curl Style)')
   .option('-u, --useragent <UserAgent>', 'User agent to inject to your query')
+  .option('-A, --user-agent <UserAgent>', 'User agent to inject to your query (Curl Style)')
+  .option('-c, --cookies <cookies>', 'Set cookie string, such "a=v1&b=v2", don t forget quotes on unix because of the &')
+  .option('-b, --cookie <name=data>', 'Pass the data to the HTTP server as a cookie. (Curl Style)')
+  .option('-d, --data <data>', 'Sends the specified data in a POST request to the HTTP server, in the same way that a browser does when a user has filled in an HTML form and presses the submit button. (Curl Style)')
+  .option('-e, --referer <url>', 'Sends the "Referrer Page" information to the HTTP server. (Curl Style)')
+  .option('-H, --header <header>', ' Extra header to include in the request when sending HTTP to a server. (Curl Style)')
+  .option('--data-raw <data>', 'This posts data similarly to --data but without the special interpretation of the @ character. (Curl Style)')
+  .option('--data-urlencode  <data>', 'This posts data, similar to the other --data options with the exception that this performs URL-encoding. (Curl Style)')
+  .option('-L, --location', 'Follow redirects. (Curl Style)')
+  .option('-o, --output <file>', 'Output to specified file. (Curl Style)')
+  .option('-u, --user <user:password>', 'HTTP Auth. (Curl Style)')
   .action(function (url, options){
+
     if(options.verbose) log.level = 'verbose';
+
     var print = function(d){
       if(!options.raw){
         d = JSON.stringify(d,null,2);
@@ -25,40 +40,73 @@ program.version(pkg.version)
       console.log( d );
     };
 
+    var method = options.method || options.request || 'GET';
+
+    var ParsedUrl = urlUtil.parse(url);
+
     var reqOptions = {
       url: url,
-      strictSSL : (!!options.strictSSL),
-      method : (!!options.method)?options.method:'GET',
-      jar: (!!options.cookies)?request.jar():null,
-      headers: {
-        'User-Agent': options.useragent?options.useragent:null
-      }
+      strictSSL : (!options.insecure),
+      method : method,
+      followRedirect : (!!options.location),
+      headers: {}
     };
 
-    console.log('====================== ' + reqOptions.url);
+    if(options.useragent){
+      reqOptions.headers['User-Agent'] = options.useragent;
+    }else if(options['user-agent']){
+      reqOptions.headers['User-Agent'] = options['user-agent'];
+    }
+
+    if(options.referer){
+      reqOptions.headers['Referer'] = options.referer;
+    }
+
+    if(options.cookies){
+      reqOptions.jar = request.jar();
+    }
+
+    if(ParsedUrl.auth){
+      reqOptions.auth = {
+        user: ParsedUrl.auth.split(":")[0],
+        pass: ParsedUrl.auth.split(":")[1]
+      };
+    }else if( options.user ){
+      reqOptions.auth = {
+        user: options.user.split(":")[0],
+        pass: options.user.split(":")[1]
+      };
+    }
+
+
+    log.info('URL ', '%s %s', method, reqOptions.url);
+
     if(options.reqheaders){
-      console.log('\n====================== Request headers');
-      console.log(reqOptions.headers );
-      console.log("strictSSL "+reqOptions.strictSSL );
-      console.log("method "+reqOptions.method );
+      log.info('REQ ', 'Request headers');
+      log.info('    ', reqOptions.headers );
+      log.info('    ', 'strictSSL %s', (!reqOptions.insecure) );
+      log.info('    ', 'method %s', reqOptions.method );
     }
     if(!!options.cookies){
-      console.log('\n====================== Request Cookies');
+      log.info('REQ ', 'Request Cookies');
       options.cookies.split('&').forEach(function cook(cookie){
-        reqOptions.jar.setCookie( request.cookie(cookie), reqOptions.url.replace(/(\?.*)$/, '') );
-        console.log(cookie)
+        reqOptions.jar.setCookie( request.cookie(cookie), '%s//%s%s', ParsedUrl.protocol, ParsedUrl.host, ParsedUrl.pathname );
+        log.info('    ', cookie);
       });
     }
+    log.info('REQ ', 'sent');
     request(reqOptions, function (error, response, body) {
+      log.info('RES ', 'received');
       if(error){
-        console.log('\n====================== Failure');
-        console.log(error);
+        log.error('RES ', 'Failure');
+        log.error(error);
       }else{
-        console.log('\n====================== Response headers');
-        console.log('status : ' + response.statusCode );
-        print(response.headers );
+        log.info('RES ', 'Code %s', response.statusCode );
+        if(options.resheaders){
+          print(response.headers );
+        }
         if(options.body){
-          console.log('\n====================== Body');
+          log.info('BODY', ' ');
           print(body );
         }
       }
@@ -66,14 +114,6 @@ program.version(pkg.version)
   });
 
 // ----- HELP
-
-program
-  .command('*')
-  .description('help')
-  .action(function(){
-    program.outputHelp();
-  });
-
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
